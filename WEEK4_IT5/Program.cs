4ed9aa524a91402bbfb83312c2ce6593a796bb62
ArtZ23 <zzartur77@gmail.com>
2023-05-02 22:37:23 +0400

    Add First Visual Studio ClassWork

---
+++ WEEK4_IT5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WEEK4_IT5
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.BackgroundColor = ConsoleColor.Green;
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("   /|");
+			Console.WriteLine("  / |");
+			Console.WriteLine(" /  |");
+			Console.WriteLine("/___|");
+			Console.ResetColor();
+		}
+	}
+}
